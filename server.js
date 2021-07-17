8c6c5fe4933284f785c2c405226e56d412464fa2
Manish Sharma <sharmamanish3@gmail.com>
2021-07-18 00:17:52 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,5 @@
+require("dotenv").config({path: "c:/dotenv/.env"});
+console.log("APP_HOST", process.env.APP_HOST);
+console.log("APP_PORT", process.env.APP_PORT);
+console.log("APP_USER", process.env.APP_USER);
+console.log("APP_PASSWORD", process.env.APP_PASSWORD);
