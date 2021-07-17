bc0ffcca1ee0e193a6546434cb885fd80b3abfca
Manish Sharma <sharmamanish3@gmail.com>
2021-07-18 00:49:07 +0530

    Added Date Functonality

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ console.log("APP_HOST", process.env.APP_HOST);
 console.log("APP_PORT", process.env.APP_PORT);
 console.log("APP_USER", process.env.APP_USER);
 console.log("APP_PASSWORD", process.env.APP_PASSWORD);
+
+setTimeout(()=>console.log("Time out"), 2000);
+console.log("Date Value", new Date().toISOString());
+console.log("Date Value 2", new Date().toDateString());
